1922ee2961df314cc2e29c5eb295b887add62bc5
changhyuni <bhs9610@naver.com>
2021-04-21 20:39:22 +0900

    0.0.1

--- app.py
+++ app.py
@@ -6,8 +6,9 @@ app = Flask(__name__)
 def Samp():
     with SampClient(address='203.248.21.223', port=7777) as client:
         info = client.get_server_info()
-        result = ''.join(('서버인원 : ', info.players ))
+        result = f"서버인원 {info.players}명", info.gamemode, info.hostname
     return result
+
 
 @app.route('/keyboard')
 def Keyboard():
@@ -21,6 +22,18 @@ def Message():
     content = request.get_json()
     content = content['userRequest']
     content = content['utterance']
+            response_data = {
+                "version": "2.0",
+                "template": {
+                    "outputs": [
+                        {"simpleText": 
+                        "text": returnMenu(ChoiceUrl, ChoiceDay)}
+                        }
+                        ],
+                    "quickReplies": [{"label": "처음으로", "action": "message", "messageText": "처음으로"},
+                                     ]
+                }
+            }
     
     if content == u"안녕":
         dataSend = {
@@ -51,6 +64,9 @@ def Message():
                             "text" : f"{Samp()}"
                         }
                     }
+                ],
+                "quickReplies": [
+                    {"label": "인원", "action": "message", "messageText": "인원"},
                 ]
             }
         }
