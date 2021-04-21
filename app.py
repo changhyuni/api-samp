30e11fe412f83caeda247c7bad767d54e22243cd
changhyuni <bhs9610@naver.com>
2021-04-21 19:40:37 +0900

    test

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 def Samp():
     with SampClient(address='203.248.21.223', port=7777) as client:
         info = client.get_server_info()
-        result = print(f"서버인원 {info.players}명", info.gamemode, info.hostname)
+        result = ''.join(('서버인원 : ', info.players ))
     return result
 
 @app.route('/keyboard')
