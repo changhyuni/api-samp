e594ec4a5539deb1d53d55bcfbea4ad65e3525f9
changhyuni <bhs9610@naver.com>
2021-04-21 16:46:22 +0900

    0.0.1

---
+++ app.py
@@ -0,0 +1,19 @@
+from samp_client.client import SampClient
+import jsonify
+from flask import Flask
+
+app = Flask(__name__)
+
+def Samp():
+    with SampClient(address='203.248.21.223', port=7777) as client:
+        info = client.get_server_info()
+        result = f"서버인원 {info.players}명", info.gamemode, info.hostname
+    return str(result)
+
+@app.route("/api")
+def hello():
+    return Samp()
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
