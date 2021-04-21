46827d13876e72f7bb78ade8566c78cca5f5241a
changhyuni <bhs9610@naver.com>
2021-04-22 02:16:37 +0900

    test

--- test.py
+++ test.py
@@ -43,7 +43,7 @@ def Message():
                 "outputs": [
                     {
                         "simpleText":{
-                            "text" : f"현재 서버인원은 {info.players} 명\n" + f"{info.gamemode} 라운드가 진행중입니다!"
+                            "text" : f"현재 서버인원은 {info.players}명\n" + f"{info.gamemode}\n 라운드가 진행중입니다!"
                         }
                     }
                 ]
