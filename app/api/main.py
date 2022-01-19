04b2195ece59167dcf4edf987d3958126e846d51
a <a@as-MacBook-Air.local>
2022-01-19 18:44:45 +0900

    Add package fastapi

--- app/api/main.py
+++ app/api/main.py
@@ -7,7 +7,7 @@ import json
 
 
 with SampClient(address='14.35.79.33', port=7777) as client:
-    ServerInfo = client.get_server_info()
+    ServerInfo = client.get_server_clients_detailed()
 
 app = FastAPI()
 
@@ -43,31 +43,5 @@ async def root(arbitrary_json: JSONStructure = None):
 @app.get('/heartbeat')
 async def health_check():                   
     return "OK"
-
-# @app.post("/send/", description="데이터 보내는 API")
-# async def send_data(send: SendIn):
-#     send = send.userRequest[0].utterance
-#     if send == '인원':
-#         content = {
-#         "version": "2.0",
-#         "template": {
-#             "outputs": [
-#                 {
-#                     "simpleText":{
-#                         "text" : f"{ServerInfo}"
-#                     }
-#                 }
-#             ],
-#             "quickReplies": [
-#                 {"label": "인원", "action": "message", "messageText": "인원"},
-#                 {"label": "도움말", "action": "message", "messageText": "도움말"},
-#                 {"label": "기능", "action": "message", "messageText": "기능"}
-#                 ]
-#             }
-#         }
-#     else:
-#         content = "x"
-        
-#     return content
 
 
---
+++ app/api/test.py
@@ -0,0 +1,6 @@
+from samp_client.client import SampClient
+
+with SampClient(address='14.35.79.33', port=7777) as client:
+    ServerInfo = client.get_server_info()
+
+print(ServerInfo)
