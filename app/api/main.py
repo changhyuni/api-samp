071c9dcc6674d02c6a3b069752bc6f0e04274993
a <a@as-MacBook-Air.local>
2022-01-19 18:09:16 +0900

    Add package fastapi

--- app/api/main.py
+++ app/api/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI, Request
 from fastapi.responses import JSONResponse
 from samp_client.client import SampClient
 from pydantic import BaseModel
-from typing import Optional, List
+from typing import Optional, List, Any, Dict, AnyStr, Union
 import json
 
 
@@ -11,41 +11,46 @@ with SampClient(address='14.35.79.33', port=7777) as client:
 
 app = FastAPI()
 
+JSONObject = Dict[AnyStr, Any]
+JSONArray = List[Any]
+JSONStructure = Union[JSONArray, JSONObject]
 
-class SendInBase(BaseModel):
-    utterance: str
 
-class SendIn(BaseModel):
-    userRequest: List[SendInBase]
+@app.post("/")
+async def root(arbitrary_json: JSONStructure = None):
+    return {"received_data": arbitrary_json}
+
+# with open('./skill.json', 'r') as f:
+#     json_data = json.load(f)
 
 @app.get('/heartbeat')
 async def health_check():                   
     return "OK"
 
-@app.post("/send/", description="데이터 보내는 API")
-async def send_data(send: SendIn):
-    send = send.userRequest[0].utterance
-    if send == '인원':
-        content = {
-        "version": "2.0",
-        "template": {
-            "outputs": [
-                {
-                    "simpleText":{
-                        "text" : f"{ServerInfo}"
-                    }
-                }
-            ],
-            "quickReplies": [
-                {"label": "인원", "action": "message", "messageText": "인원"},
-                {"label": "도움말", "action": "message", "messageText": "도움말"},
-                {"label": "기능", "action": "message", "messageText": "기능"}
-                ]
-            }
-        }
-    else:
-        content = "x"
+# @app.post("/send/", description="데이터 보내는 API")
+# async def send_data(send: SendIn):
+#     send = send.userRequest[0].utterance
+#     if send == '인원':
+#         content = {
+#         "version": "2.0",
+#         "template": {
+#             "outputs": [
+#                 {
+#                     "simpleText":{
+#                         "text" : f"{ServerInfo}"
+#                     }
+#                 }
+#             ],
+#             "quickReplies": [
+#                 {"label": "인원", "action": "message", "messageText": "인원"},
+#                 {"label": "도움말", "action": "message", "messageText": "도움말"},
+#                 {"label": "기능", "action": "message", "messageText": "기능"}
+#                 ]
+#             }
+#         }
+#     else:
+#         content = "x"
         
-    return content
+#     return content
 
 
