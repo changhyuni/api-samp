58cc7d1bc76f326b996e64bfc4434bb4ac021162
changhyuni <bhs9610@naver.com>
2022-04-03 19:07:39 +0900

    modify 1 server

--- app/api/main.py
+++ app/api/main.py
@@ -20,19 +20,24 @@ async def health_check():
 @app.post("/")
 async def root(arbitrary_json: JSONStructure = None):
 
-    UserList = ''
+### 1 Server
+###
+
+    # UserList = ''
+    # with SampClient(address='14.35.79.33', port=32085) as client:
+    #     ServerInfo = client.get_server_info()
+    #     UserInfo = client.get_server_clients()
+
+    # for User in UserInfo:
+    #     UserList += User.name + '\n'
+
+### 2 Sever
+###
+
     TwoUserList = ''
-    with SampClient(address='14.35.79.33', port=6969) as client:
-        ServerInfo = client.get_server_info()
-        UserInfo = client.get_server_clients()
-
-    for User in UserInfo:
-        UserList += User.name + '\n'
-
     with SampClient(address='14.35.79.33', port=32085) as client:
         TwoServerInfo = client.get_server_info()
         TwoUserInfo = client.get_server_clients()
-
 
     for TwoUser in TwoUserInfo:
         TwoUserList += TwoUser.name + '\n'
@@ -46,14 +51,14 @@ async def root(arbitrary_json: JSONStructure = None):
 
     Tworound_info = '대기' if TwoServerInfo.gamemode == 'Att-Def v1.23 (r)' else '게임'
     round_info = '대기' if TwoServerInfo.gamemode == 'Att-Def v1.23 (r)' else '게임'
-
+# "\n2서버에 "f'{Twoplayers}'"명이 " f'{Tworound_info} ' "중입니다! \n\n<유저리스트>\n"f'{TwoUserList}'"
     content = {
         "version": "2.0",
         "template": {
             "outputs": [
                 {
                     "simpleText":{
-                        "text" : "1서버에 "f'{players}'"명이 " f'{round_info} ' "중입니다! \n\n<유저리스트>\n"f'{UserList}'"\n2서버에 "f'{Twoplayers}'"명이 " f'{Tworound_info} ' "중입니다! \n\n<유저리스트>\n"f'{TwoUserList}'""
+                        "text" : "1서버에 "f'{players}'"명이 " f'{round_info} ' "중입니다! \n\n<유저리스트>\n"f'{UserList}'""
 
                 }
               }
--- app/api/test.py
+++ app/api/test.py
@@ -1,15 +1,92 @@
+#!/usr/bin/env python
+# Usage: `python example.py (address=localhost) (port=7777) (rcon_password)`
+# Example script demonstrating the use of this library
+#
+import sys
+from builtins import input
+
 from samp_client.client import SampClient
 
 
-with SampClient(address='14.35.79.33', port=6969, rcon_password='nclubkorea123') as client:
-    info = client.get_server_info()
-    UserInfo = client.get_server_clients()
+def info(client):
+    print("""Server Info:
+Password: {info.password}
+Players: {info.players}/{info.max_players}
+Hostname: {info.hostname}
+Gamemode: {info.gamemode}
+Language: {info.language}
+    """.format(
+        info=client.get_server_info(),
+    ))
 
-    print(info)
-    print(UserInfo)
+
+def rules(client):
+    print("Server Rules:")
+    for rule in client.get_server_rules():
+        print("{rule.name}: {rule.value}".format(
+            rule=rule,
+        ))
 
 
-# with SampClient(address='14.35.79.33', port=32085) as client:
-#         TwoServerInfo = client.get_server_info()
-#         print(TwoServerInfo)
+def clients(client):
+    print("Connected Clients")
+    print("Name                       | Score".format(client=client))
+    print("==================================")
+    for client in client.get_server_clients():
+        print("{client.name:26} | {client.score}".format(client=client))
 
+
+def details(client):
+    print("Detailed Clients")
+    print(" ID |Name                        | Score | Ping".format(client=client))
+    print("===============================================")
+    for client in client.get_server_clients_detailed():
+        print("{client.id:3} | {client.name:26} | {client.score:5} | {client.ping:4}".format(client=client))
+
+
+def rcon(client):
+    if client.rcon_password is None:
+        client.rcon_password = input('RCON password:')
+    print('Enter rcon commands or leave blank to exit. Example: cmdlist')
+    while True:
+        command = input('RCON: ')
+        if not command:
+            return
+        for line in client.send_rcon_command(command):
+            print(line)
+
+
+def main(args):
+    with SampClient(*args) as client:
+        if not client.is_online():
+            print('Server {}:{} is offline'.format(*args))
+            exit(1)
+        server_info = client.get_server_info()
+        print("""Connected to {info.hostname}. 
+Currently {info.players}/{info.max_players} players online.
+Select one of the options:
+i. Server Info
+r. Server Rules
+c. Connected clients
+d. Detailed clients
+x. RCON
+""".format(
+            info=server_info,
+        ))
+
+        options = {
+            'i': info,
+            'r': rules,
+            'c': clients,
+            'd': details,
+            'x': rcon,
+        }
+        option = input('Select option: ')
+        if option in options: options[option](client)
+        else: print('Unknown option, bye!')
+
+
+if len(sys.argv) < 3:
+    print('Usage: python example.py [server_address] [port]')
+    exit(1)
+main(sys.argv[1:])
